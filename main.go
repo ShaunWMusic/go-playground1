d8e50c15bdf0c5353fc576216f243520fa7b7745
Shaun Willis <swillis@nabancard.com>
2021-02-18 19:04:40 -0600

    add articles as first json endpoint

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title string `json:"Title`
+	Desc string `json:"desc`
+	Content string `json:"content`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title:"Test Title", Desc:"Test Description", Content: "Hello World"},
+	}
+	fmt.Println("Endpoint Hit: All Articles Endpoint 🎉")
+	json.NewEncoder(w).Encode(articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Homepage Endpoint Hit🎉")
@@ -12,6 +29,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequest() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
